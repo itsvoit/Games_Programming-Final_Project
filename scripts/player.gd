97694cdc0a52de99da627c7d452b90ffc1cd3c4b
skowron4 <adam.skowron44@gmail.com>
2024-05-27 22:26:38 +0200

    create basic player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animated_sprite = $AnimatedSprite2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-@onready var health = $Health
-@onready var animation_player = $AnimationPlayer
-
-func take_damage(value):
-	health.take_damage(value)
-
+var current_speed = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,20 +16,41 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction > 0:
+		if current_speed < SPEED:
+			current_speed += 20
+		velocity.x = current_speed
+	elif direction < 0:
+		if current_speed > -SPEED:
+			current_speed -= 20
+		velocity.x = current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		current_speed = move_toward(current_speed, 0, 10)
+		velocity.x = current_speed
 	
-	# Handle attack
-	if Input.is_action_just_pressed("light_attack"):
-		animation_player.play("light_attack")
-		print("light_attack")
+    # dash
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.x = direction * 1000
+		
 	
+	if is_on_floor():
+		
+		if direction < 0:
+			animated_sprite.flip_h = true
+			animated_sprite.play("run")
+		elif direction > 0:
+			animated_sprite.flip_h = false
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+	else:
+		animated_sprite.play("jump")
+	
+
 	move_and_slide()
