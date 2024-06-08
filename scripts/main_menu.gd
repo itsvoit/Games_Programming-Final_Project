741ed0e3733dbd20770cb85eee36fbb9c3412d39
Wojciech Skrzypiec <wojciech.skrzypiec2@gmail.com>
2024-06-09 00:52:10 +0200

    Create main menu and settings

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/Start_button.grab_focus()
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/settings.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
